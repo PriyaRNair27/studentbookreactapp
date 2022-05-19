f987d645145e41683ee61a83c2148c56e895a560
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-20 04:04:16 +0530

    nav

--- src/Component/Register.js
+++ src/Component/Register.js
@@ -13,6 +13,7 @@ const Register = () => {
 
   return (
     <div>
+     
       <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
